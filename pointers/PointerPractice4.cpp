c229607dd42a4cc97a754516e743c442f03fc5f3
Pedro Navarrete <pnavarrete@harding.edu>
2018-02-16 10:48:46 -0600

    C Pointers, no references

---
+++ pointers/PointerPractice4.cpp
@@ -0,0 +1,23 @@
+// Swap two pieces of data via pointer
+
+#include <iostream>
+
+using namespace std;
+
+void Swap(int *, int *);
+
+void main() {
+	int value1 = 25;
+	int value2 = 42;
+
+	
+	
+	
+	Swap(&value1, &value2);
+}
+
+void Swap(int *p1, int *p2) {
+	int temp = *p1;
+	*p1 = *p2;
+	*p2 = temp;
+}
